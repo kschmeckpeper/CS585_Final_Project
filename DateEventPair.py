8dd231dbc76261626de79dd6122dc0fa0ba0a6a8
kschmeckpeper <kschmeckpeper@umass.edu>
2016-11-01 18:09:56 -0400

    Rewrote reuters parser

--- DateEventPair.py
+++ DateEventPair.py
@@ -1,55 +1,37 @@
 # -*- coding: utf-8 -*-
 import os, datetime, re
-
+from dateutil.parser import parse
 def read_reuters(path):
     pairs = []
 
     for filename in os.listdir(path):
         if filename.endswith('.sgm'): #reuters
             #print filename
-            files = open(path + filename).read().strip()
-            if files.find("<BODY>") != -1:
-                d_start = files.find("<DATE>")
-                d_end = files.find("</DATE>")
-                dt = files[d_start:d_end]
-                date = re.search("(\d+)-(\w+)-(\d+)", dt).group(0)
-                day = re.search("(\d+)", date).group(0)
-                month = re.search("(\D+)", date).group(0)
-                month = month[1:-1]
-                year = 1987
-                if month=="JAN":
-                    month = 1
-                elif month=="FEB":
-                    month = 2
-                elif month=="MAR":
-                    month = 3
-                elif month=="APR":
-                    month = 4
-                elif month=="MAY":
-                    month = 5
-                elif month=="JUN":
-                    month = 6
-                elif month=="JUL":
-                    month = 7
-                elif month=="AUG":
-                    month = 8
-                elif month=="SEP":
-                    month = 9
-                elif month=="OCT":
-                    month = 10
-                elif month=="NOV":
-                    month = 11
-                elif month=="DEC":
-                    month = 12
-                obj = datetime.date(year, month, int(day))
-                c_start = files.find("<BODY>")
-                c_end = files.find("</REUTERS>")
-                body = files[c_start:c_end]
-                content = body[6:]
-                pairs.append((obj, content))
-                files = files[c_end:]
+            file = open(path + os.path.sep + filename).read().strip()
+
+            curr_index = 0
+            while file.find("<REUTERS", curr_index) != -1:
+            	date_start = file.find("<DATE>", curr_index)
+            	date_start = file.find(">", date_start) + 1
+            	date_end = date_start + 11
+            	date_string = file[date_start:date_end]
+            	date = parse(date_string)
+            	
+
+            	body_start = file.find("<BODY>", curr_index) + 6
+            	body_end = file.find("</BODY>", curr_index)
+            	
+            	curr_index = file.find("</REUTERS>", curr_index) + 1
+
+            	if body_start > curr_index or body_start == -1:
+            		continue
+
+            	text = file[body_start:body_end]
+            	
+            	pairs.append((date, text))
+
     return pairs
 
 if __name__ == '__main__':
-    pairs = read_reuters('/Users/EvanReiff/Desktop/Senior 1/CMPSCI 585/Project/reuters21578/')
+    pairs = read_reuters('reuters')
     print pairs
